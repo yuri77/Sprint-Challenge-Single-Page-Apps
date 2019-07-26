b84332426ea6421b8a4d5f859fb7595113e01cde
yurika sakae <yurika.sun@gmail.com>
2019-07-26 10:07:15 -0700

    axios call added to Character page

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+//import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [char, setChar] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  // useEffect(() => {
+  //   const getCharacters = () => {
+  //     axios
+  //       .get("https://rickandmortyapi.com/api/character/")
+  //       .then(response => {
+  //         console.log("characters: ", response);
+  //       })
+  //       .catch(error => {
+  //         console.log("Character fetching is currently down", error);
+  //       });
+  //   };
+  //   getCharacters();
+  // }, []);
 
-  return <section className='character-list grid-view'>
-
+  return (
+    <section className="character-list grid-view">
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
-
+  );
 }
